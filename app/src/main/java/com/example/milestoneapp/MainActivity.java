f4674f1dd0739d648515f9117406356b152c1387
raulito <raul.ramos.1992.1992@gmail.com>
2022-12-30 11:16:38 -0600

    Main Activity solo hace el bind del layout, comparar con commit anterior

---
+++ app/src/main/java/com/example/milestoneapp/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.milestoneapp;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText username;
+    EditText password;
+    Button loginButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+}
