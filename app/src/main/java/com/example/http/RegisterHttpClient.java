b1561fdd21bf7588e908b0023c04e1a4678f356d
raulito <raul.ramos.1992.1992@gmail.com>
2023-01-04 00:15:26 -0600

    Se realiza petición con json hardcodeado pero funcional

--- app/src/main/java/com/example/http/RegisterHttpClient.java
+++ app/src/main/java/com/example/http/RegisterHttpClient.java
@@ -9,6 +9,7 @@ import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
 import okhttp3.FormBody;
+import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
@@ -23,7 +24,7 @@ public class RegisterHttpClient {
     private OkHttpClient client;
     private Gson gson;
 
-    private static final String REGISTRAR_URL = "http://localhost:8080/api/usuarios";
+    private static final String REGISTRAR_URL = "https://springgcp-373701.ue.r.appspot.com/api/usuarios";
 
     private RegisterHttpClient() {
         executor = Executors.newFixedThreadPool(4);
@@ -34,16 +35,20 @@ public class RegisterHttpClient {
     public void registrarUsuario(RegisterHttpCallback callback) {
 
         RequestBody formBody = new FormBody.Builder()
-                .add("nombre", "denisse")
-                .add("apellido", "garcia")
+                .add("nombre", "bernabeu")
+                .add("apellido", "panda")
                 .add("correo", "corruu@hotmail.com")
                 .add("password", "contrita")
                 .add("rol", "fancy")
                 .build();
 
+        String jjson = "{\"nombre\":\"Luzmaria\",\"apellido\":\"Almita\",\"correo\":\"corruu@hotmail.com\",\"password\":\"contrita\",\"rol\":\"rol\"}";
+
+        RequestBody otroBody=RequestBody.create(MediaType.parse("application/json"), jjson);
+
         Request request = new Request.Builder()
                 .url(REGISTRAR_URL)
-                .post(formBody)
+                .post(otroBody)
                 .build();
 
         executor.execute(() -> {
